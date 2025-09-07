9333568b8dc4698eaaa730bdefc331cc93a20166
Meytardzhevtd <meytardzhevtd@yandex.ru>
2025-09-07 06:12:04 +0300

    sadasd

--- src/NFA.cpp
+++ src/NFA.cpp
@@ -1,4 +1,9 @@
 #include "../inc/NFA.hpp"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
 
 namespace homework_nfa {
 std::vector<int> parce_string(std::string input_string, char delimiter) {
@@ -54,32 +59,14 @@ void NFA::read_file(const std::string &file_path) {
 }
 
 [[nodiscard]] bool NFA::simulate(const std::string &input) const {
-    {
-        for (auto p : transitions) {
-            std::cout << "{" << p.first.first << ", " << p.first.second << "} -> {";
-            for (int num : p.second) {
-                std::cout << num << ", ";
-            }
-            std::cout << "}\n";
-        }
-    }
-
     std::set<int> current_states = start_state_id;
 
-    {
-        std::cout << "Start states: ";
-        for (int i : current_states) {
-            std::cout << i << " ";
-        }
-        std::cout << std::endl;
-    }
     for (const char c : input) {
         const int number = c - '0';
         std::set<int> new_states;
 
         for (const int current_state : current_states) {
-            if (transitions.find({current_state, number}) !=
-                transitions.end()) {
+            if (transitions.count({current_state, number})) {
                 const auto &next_states =
                     transitions.at({current_state, number});
                 for (const int state : next_states) {
@@ -88,16 +75,9 @@ void NFA::read_file(const std::string &file_path) {
             }
         }
         current_states = new_states;
-        {
-            std::cout << "Current states: ";
-            for (int i : current_states) {
-                std::cout << i << " ";
-            }
-            std::cout << std::endl;
-        }
     }
     for (const int final_state : final_state_id) {
-        if (current_states.count(final_state) != 0) {
+        if (current_states.count(final_state)) {
             return true;
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,49 @@
 #include "../inc/NFA.hpp"
 #include "../inc/doctest.h"
 
-// TEST_CASE("Task 1 Test 1") {
-//     homework_nfa::NFA nfa;
-//     nfa.read_file("../tests/Test11.txt");
-//     CHECK(nfa.simulate("0") == false);
-//     CHECK(nfa.simulate("01") == false);
-//     CHECK(nfa.simulate("00") == true);
-//     CHECK(nfa.simulate("") == false);
-// }
+TEST_CASE("Task 1 Test 1") {
+    homework_nfa::NFA nfa;
+    nfa.read_file("../tests/Test11.txt");
+    CHECK(nfa.simulate("0") == false);
+    CHECK(nfa.simulate("01") == false);
+    CHECK(nfa.simulate("00") == true);
+    CHECK(nfa.simulate("") == false);
+}
 
-// TEST_CASE("Task 1 Test 2") {
-//     homework_nfa::NFA nfa;
-//     nfa.read_file("../tests/Test12.txt");
-//     CHECK(nfa.simulate("1") == true);
-//     CHECK(nfa.simulate("01") == true);
-//     CHECK(nfa.simulate("001") == true);
-//     CHECK(nfa.simulate("000") == false);
-// }
+TEST_CASE("Task 1 Test 2") {
+    homework_nfa::NFA nfa;
+    nfa.read_file("../tests/Test12.txt");
+    CHECK(nfa.simulate("1") == true);
+    CHECK(nfa.simulate("01") == true);
+    CHECK(nfa.simulate("001") == true);
+    CHECK(nfa.simulate("000") == false);
+}
 
-// TEST_CASE("Task 1 Test 3") {
-//     homework_nfa::NFA nfa;
-//     nfa.read_file("../tests/Test13.txt");
-//     CHECK(nfa.simulate("") == true);
-//     CHECK(nfa.simulate("0") == true);
-//     CHECK(nfa.simulate("000") == true);
-//     CHECK(nfa.simulate("1") == false);
-// }
+TEST_CASE("Task 1 Test 3") {
+    homework_nfa::NFA nfa;
+    nfa.read_file("../tests/Test13.txt");
+    CHECK(nfa.simulate("") == true);
+    CHECK(nfa.simulate("0") == true);
+    CHECK(nfa.simulate("000") == true);
+    CHECK(nfa.simulate("1") == false);
+}
 
-// TEST_CASE("Task 1 Test 4") {
-//     homework_nfa::NFA nfa;
-//     nfa.read_file("../tests/Test14.txt");
-//     CHECK(nfa.simulate("0") == false);
-//     CHECK(nfa.simulate("00") == true);
-//     CHECK(nfa.simulate("1") == true);
-//     CHECK(nfa.simulate("01") == true);
-// }
+TEST_CASE("Task 1 Test 4") {
+    homework_nfa::NFA nfa;
+    nfa.read_file("../tests/Test14.txt");
+    CHECK(nfa.simulate("0") == false);
+    CHECK(nfa.simulate("00") == true);
+    CHECK(nfa.simulate("1") == true);
+    CHECK(nfa.simulate("01") == true);
+}
 
-TEST_CASE("Task 1 Test 5 - Complex NFA") {
+TEST_CASE("Task 1 Test 5") {
     homework_nfa::NFA nfa;
     nfa.read_file("../tests/Test15.txt");
-    // CHECK(nfa.simulate("03") == true);  
-    // CHECK(nfa.simulate("214") == false);    
-    CHECK(nfa.simulate("2") == true);     
-  
+    CHECK(nfa.simulate("03") == true);
+    CHECK(nfa.simulate("214") == false);
+    CHECK(nfa.simulate("52") == false);
+    CHECK(nfa.simulate("2314442303211111111") == true);
+    CHECK(nfa.simulate("1440222222") == false);
+    CHECK(nfa.simulate("2301022222") == false);
 }
